75fa5e9be75cefbde6f7bf9d7df3c6fe5f9906a2
caio20538 <caio20538@gmail.com>
2025-02-28 13:01:24 -0300

    Projeto de SmartTV aplicando conhecimentos Basicos

---
+++ src/edu/caio/javabasico/SmartTV.java
@@ -0,0 +1,12 @@
+package edu.caio.javabasico;
+
+public class SmartTV {
+	//exercício aplicando os conhecimentos de métodos e variáveis
+	
+	//atributos
+	boolean ligada=false;
+	int canal=1;
+	int volume=24;
+	
+	
+}
---
+++ src/edu/caio/javabasico/Usuario.java
@@ -0,0 +1,10 @@
+package edu.caio.javabasico;
+
+public class Usuario {
+
+	public static void main(String[] args) {
+		SmartTV smartTv = new SmartTV();
+		
+		System.out.println(smartTv.ligada);
+	}
+}
