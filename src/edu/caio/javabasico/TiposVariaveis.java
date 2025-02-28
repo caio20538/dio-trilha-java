edd2dff5eb663eabc03e5a8c83ed95bdf4351a94
caio20538 <caio20538@gmail.com>
2025-02-28 12:15:01 -0300

    Concluido o tipo de variáveis

--- src/edu/caio/javabasico/TiposVariaveis.java
+++ src/edu/caio/javabasico/TiposVariaveis.java
@@ -1,13 +1,15 @@
-package java_basico;
+package edu.caio.javabasico;
 
 public class TiposVariaveis {
-	
 	public static void main (String[] args) {
+		
+		//variáveis, são armazenadas em um espaço da memória, associados a um nome, que pode armazenar
+		//valores de um determminado tipo
 		int idade; 
 		int dataFabricacao = 2001;
 		long cpf = 98944462107L;
 		double salarioMinimo = 2500.00;
-		final float PI = 3.14f;
+		float pi = 3.14f;
 		
 		short numeroCurto = 1;
 		int numeroNormal = numeroCurto;
@@ -21,6 +23,9 @@ public class TiposVariaveis {
 		
 		short numeroCurto2 = (short)numeroNormal; //saida: 1
 		//faz o casting transformando em short
+		
+		//constantes, similar as variáveis, mas não podem ser modificadas depois de declaradas
+		final float VALOR_DE_PI = 3.14f;
 		
 		
 	}
